56d5ff58c49c4b09c73d6e80ab2701b9b026a341
Emily Dixon <edixon@mux.com>
2023-01-19 16:11:33 -0800

    Add initial project with placeholders

---
+++ app/src/main/java/com/example/muxvideovoduploadsdk/PlaceholderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.muxvideovoduploadsdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaceholderActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placeholder)
+  }
+}
