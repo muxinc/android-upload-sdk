caae25e37747f16b5a8436c69ec5a8c5cac423c2
Emily Dixon <edixon@mux.com>
2023-02-02 12:13:04 -0800

    Ok we can at least see if it lists

--- app/src/main/java/com/mux/video/vod/demo/DeviceStoreVideosViewModel.kt
+++ app/src/main/java/com/mux/video/vod/demo/DeviceStoreVideosViewModel.kt
@@ -1,16 +1,19 @@
 package com.mux.video.vod.demo
 
 import android.app.Application
-import androidx.lifecycle.*
+import android.database.Cursor
+import android.os.Build
+import android.provider.MediaStore
+import android.provider.MediaStore.Video.VideoColumns
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.mux.video.vod.demo.model.DeviceStoreVideo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.BufferedInputStream
-import java.io.BufferedOutputStream
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 
 /**
  * Queries the device's content provider for saved videos to upload
@@ -31,7 +34,45 @@ class DeviceStoreVideosViewModel(private val app: Application) : AndroidViewMode
   }
 
   private suspend fun fetchVideos(): List<DeviceStoreVideo> {
-    return listOf()
+    fun ownerPackageName(cursor: Cursor): String {
+      return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        cursor.getString(VideoColumns.OWNER_PACKAGE_NAME) ?: "??"
+      } else {
+        "??"
+      }
+    }
+
+    val cursor = withContext(Dispatchers.IO) {
+      app.contentResolver.query(
+        MediaStore.Video.Media.INTERNAL_CONTENT_URI,
+        /* projection = */ null,
+        /* selection = */ null,
+        /* selectionArgs = */ null,
+        /* sortOrder = */ MediaStore.Video.VideoColumns.DISPLAY_NAME + " DESC"
+      )!!
+    }
+
+    val videos = mutableListOf<DeviceStoreVideo>()
+    cursor.moveToFirst()
+    do {
+      val title = cursor.getString(VideoColumns.DISPLAY_NAME) ?: "[no name]"
+      val file = cursor.getString(VideoColumns.DATA) ?: continue
+      val fromApp = cursor.getString(VideoColumns.OWNER_PACKAGE_NAME) ?: "??"
+
+      val vid = DeviceStoreVideo(
+        title = title,
+        file = File(file),
+        fromApp = fromApp,
+      )
+      videos += vid
+    } while (cursor.moveToNext())
+
+    return videos
+  }
+
+  private fun Cursor.getString(columnName: String): String? {
+    val colIdx = getColumnIndexOrThrow(columnName)
+    return getString(colIdx)
   }
 
   /**
@@ -39,31 +80,32 @@ class DeviceStoreVideosViewModel(private val app: Application) : AndroidViewMode
    * temp directory. (Technically we could stream it from the source, but this prevents the other
    * app from modifying the file if we pause the upload for a long time (or whatever)
    *
-   * TODO: The SDK should do this probably. Most ugc apps won't need it, but any app that wants to
+   * TODO: Wait we don't really have to do this! Whoohoo!
+   * TODO<em>: Should the SDK do this? Most ugc apps won't need it, but any app that wants to
    *  interact with the device Media Store (different from local flat files) needs to do this step
    */
-  @Throws
-  private suspend fun copyIntoTempFile(video: DeviceStoreVideo): File {
-    val basename = video.contentUri.pathSegments.joinToString(separator = "-")
-    val cacheDir = File(app.cacheDir, "mux-upload")
-    cacheDir.mkdirs()
-    val destFile = File(cacheDir, basename)
-
-    withContext(Dispatchers.IO) {
-      val input = FileInputStream(video.fileDescriptor.fileDescriptor).channel
-      val output = FileOutputStream(destFile).channel
-      val fileSize = input.size()
-      try {
-        var read = 0L
-        do {
-          read += input.transferTo(read, 10 * 1024, output)
-        } while (read < fileSize)
-      } finally {
-        input.close()
-        output.close()
-      }
-    }
-
-    return destFile
-  }
+//  @Throws
+//  private suspend fun copyIntoTempFile(video: DeviceStoreVideo): File {
+//    val basename = video.contentUri.pathSegments.joinToString(separator = "-")
+//    val cacheDir = File(app.cacheDir, "mux-upload")
+//    cacheDir.mkdirs()
+//    val destFile = File(cacheDir, basename)
+//
+//    withContext(Dispatchers.IO) {
+//      val input = FileInputStream(video.fileDescriptor.fileDescriptor).channel
+//      val output = FileOutputStream(destFile).channel
+//      val fileSize = input.size()
+//      try {
+//        var read = 0L
+//        do {
+//          read += input.transferTo(read, 10 * 1024, output)
+//        } while (read < fileSize)
+//      } finally {
+//        input.close()
+//        output.close()
+//      }
+//    }
+//
+//    return destFile
+//  }
 }
--- app/src/main/java/com/mux/video/vod/demo/model/DeviceStoreVideo.kt
+++ app/src/main/java/com/mux/video/vod/demo/model/DeviceStoreVideo.kt
@@ -2,13 +2,14 @@ package com.mux.video.vod.demo.model
 
 import android.net.Uri
 import android.os.ParcelFileDescriptor
+import java.io.File
 
 /**
  * Represents a video saved on the device, accessed via a ContentResolver. To use with the SDK, we
  * need to copy the file to our local storage
  */
 data class DeviceStoreVideo (
-  val contentUri: Uri,
   val title: String,
-  val fileDescriptor: ParcelFileDescriptor
+  val file: File,
+  val fromApp: String,
   )
