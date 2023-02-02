8ce92dcee715d107df063aab7381c8fc02fbd7c3
Emily Dixon <edixon@mux.com>
2023-02-02 14:24:57 -0800

    Request storage permission so that we can actually query videos

--- app/src/main/java/com/mux/video/vod/demo/DeviceStoreVideosActivity.kt
+++ app/src/main/java/com/mux/video/vod/demo/DeviceStoreVideosActivity.kt
@@ -1,16 +1,32 @@
 package com.mux.video.vod.demo
 
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.mux.video.vod.demo.databinding.ActivityVideoListBinding
 
 class DeviceStoreVideosActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityVideoListBinding
   private val viewModel by viewModels<DeviceStoreVideosViewModel>()
+  private val requestPermissions =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+    { grantedPermissions ->
+      if (!grantedPermissions.containsKey(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        || grantedPermissions.containsKey(android.Manifest.permission.READ_MEDIA_VIDEO)
+      ) {
+        maybeRequestPermissionsApi33()
+      } else {
+        viewModel.refresh()
+      }
+    }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -30,5 +46,50 @@ class DeviceStoreVideosActivity : AppCompatActivity() {
       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
         .setAction("Action", null).show()
     }
+
+    maybeRequestPermissions()
   }
+
+  private fun maybeRequestPermissions() {
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      maybeRequestPermissionsApi33()
+    } else {
+      maybeRequestPermissionsOld()
+    }
+  }
+
+  private fun maybeRequestPermissionsOld() {
+    val hasReadStorage =
+      ActivityCompat.checkSelfPermission(
+        this,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+      ) == PackageManager.PERMISSION_GRANTED
+
+    if (!hasReadStorage) {
+      requestPermissions.launch(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE))
+    }
+  }
+
+  @TargetApi(Build.VERSION_CODES.TIRAMISU)
+  private fun maybeRequestPermissionsApi33() {
+    val hasReadStorage =
+      ActivityCompat.checkSelfPermission(
+        this,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+      ) == PackageManager.PERMISSION_GRANTED
+    val hasReadVideo = ActivityCompat.checkSelfPermission(
+      this,
+      android.Manifest.permission.READ_MEDIA_VIDEO
+    ) == PackageManager.PERMISSION_GRANTED
+
+    if (!hasReadVideo || !hasReadStorage) {
+      requestPermissions.launch(
+        arrayOf(
+          android.Manifest.permission.READ_EXTERNAL_STORAGE,
+          android.Manifest.permission.READ_MEDIA_VIDEO
+        )
+      )
+    }
+  }
+
 }
