d1536254f2d422b643dea812d3070a862b0b2ef1
Emily Dixon <edixon@mux.com>
2023-02-02 09:02:38 -0800

    Add Viewmodel

---
+++ app/src/main/java/com/mux/video/vod/demo/VideoListViewModel.kt
@@ -0,0 +1,9 @@
+package com.mux.video.vod.demo
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Queries the device's content provider for saved videos to upload
+ */
+class VideoListViewModel : ViewModel() {
+}
