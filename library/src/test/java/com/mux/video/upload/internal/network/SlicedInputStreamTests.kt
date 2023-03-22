7ed087669d10c2a82a8a4c66812070accee9529c
Emily Dixon <edixon@mux.com>
2023-03-21 17:14:25 -0700

    Test SlicedInputStream

--- library/src/test/java/com/mux/video/upload/internal/network/SlicedInputStreamTests.kt
+++ library/src/test/java/com/mux/video/upload/internal/network/SlicedInputStreamTests.kt
@@ -1,17 +1,73 @@
 package com.mux.video.upload.internal.network
 
 import com.mux.exoplayeradapter.AbsRobolectricTest
-import io.mockk.mockk
-import io.mockk.verify
-import io.mockk.verifySequence
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.ByteArrayInputStream
 
 class SlicedInputStreamTests : AbsRobolectricTest() {
 
   @Test
-  fun testReadsUpToSliceLen() {
-    val mockReadCallback = mockk<(Long) -> Unit>(relaxed = true)
-    val dummyData = ByteArrayInputStream(ByteArray(150))
+  fun testReadsBuffersUpToSliceLen() {
+    val dummyData = ByteArrayInputStream(ByteArray(101))
+
+    val slice = dummyData.sliceOf(50)
+    val readBytes = slice.read(ByteArray(60)) // 10 bytes bigger than the slice
+    assertEquals(
+      50,
+      readBytes
+    )
+    assertEquals(
+      dummyData.available(),
+      51
+    )
+  }
+
+  @Test
+  fun testReadsToOffsetUpToSliceLen() {
+    val dummyData = ByteArrayInputStream(ByteArray(101))
+
+    val firstSlice = dummyData.sliceOf(50)
+    val firstRead = firstSlice.read(ByteArray(60), 5, 50) // 10 bytes bigger than the slice
+    assertEquals(
+      50,
+      firstRead
+    )
+    assertEquals(
+      dummyData.available(),
+      51
+    )
+  }
+
+  @Test
+  fun testReadsWithOffsetNotEnoughReadBuffer() {
+    val dummyData = ByteArrayInputStream(ByteArray(101))
+
+    val firstSlice = dummyData.sliceOf(50)
+    val firstRead = firstSlice.read(ByteArray(60), 40, 10)
+    assertEquals(
+      10,
+      firstRead
+    )
+    assertEquals(
+      dummyData.available(),
+      91
+    )
+  }
+
+  @Test
+  fun testReadsWholeBufferNotEnoughReadBuffer() {
+    val dummyData = ByteArrayInputStream(ByteArray(101))
+
+    val firstSlice = dummyData.sliceOf(50)
+    val firstRead = firstSlice.read(ByteArray(10))
+    assertEquals(
+      10,
+      firstRead
+    )
+    assertEquals(
+      dummyData.available(),
+      91
+    )
   }
 }
