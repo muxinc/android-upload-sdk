57f45b24e5b295ad4edcabbf99557b0d11b95571
Emily Dixon <edixon@mux.com>
2023-04-12 15:46:58 -0700

    Compose is really nice

--- app/src/main/java/com/mux/video/vod/demo/upload/CreateUploadActivity.kt
+++ app/src/main/java/com/mux/video/vod/demo/upload/CreateUploadActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import com.mux.video.vod.demo.R
 import com.mux.video.vod.demo.upload.ui.theme.MuxUploadSDKForAndroidTheme
 
 class CreateUploadActivity : ComponentActivity() {
@@ -19,7 +24,7 @@ class CreateUploadActivity : ComponentActivity() {
       MuxUploadSDKForAndroidTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          ScreenContent(closeThisScreen = { finish() })
         }
       }
     }
@@ -27,19 +32,53 @@ class CreateUploadActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScreenContent() {
-
+fun ScreenContent(closeThisScreen: () -> Unit = {}) {
+  return Scaffold(
+    topBar = { AppBar(closeThisScreen, true /*TODO*/) },
+  ) { contentPadding ->
+    Text("asflkjh", modifier = Modifier.padding(contentPadding))
+  }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun AppBar(closeThisScreen: () -> Unit, fileAvailable: Boolean) {
+  TopAppBar(
+    title = { Text(text = stringResource(R.string.title_activity_create_upload)) },
+    navigationIcon = {
+      IconButton(onClick = {
+        closeThisScreen() // Since we're not using Compose for everything, improvise the nav
+      },
+      enabled = fileAvailable) {
+        Icon(
+          Icons.Filled.Close,
+          contentDescription = stringResource(id = android.R.string.cancel)
+        )
+      }
+    },
+    actions = {
+      TextButton(onClick = {
+        handleCreateUpload()
+        closeThisScreen()
+      }) {
+        Text(
+          text = stringResource(id = R.string.action_create_upload),
+          style = TextStyle(
+            color = MaterialTheme.colors.onPrimary
+          )
+        )
+      }
+    },
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   MuxUploadSDKForAndroidTheme {
-    Greeting("Android")
+    ScreenContent()
   }
+}
+
+private fun handleCreateUpload() {
+  // TODO: Make the ViewModel start the MuxUpload and then close the screen
 }
