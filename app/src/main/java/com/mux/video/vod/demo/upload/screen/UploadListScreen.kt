c20a8d4e26e07556c87bd2dd5123a7b5d666e2b8
Emily Dixon <edixon@mux.com>
2023-04-14 10:29:09 -0700

    Add Upload List

---
+++ app/src/main/java/com/mux/video/vod/demo/upload/screen/UploadListScreen.kt
@@ -0,0 +1,13 @@
+package com.mux.video.vod.demo.upload.screen
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mux.video.vod.demo.upload.viewmodel.UploadListViewModel
+
+@Composable
+fun UploadListScreen() {
+
+}
+
+@Composable
+private fun screenViewModel(): UploadListViewModel = viewModel()
