3cb58137e3378b7207ec2445cfb49af2609f671b
Emily Dixon <edixon@mux.com>
2023-07-24 15:55:51 -0700

    Add helper class for java users to interpret Result

---
+++ library/src/main/java/com/mux/video/upload/api/UploadResult.kt
@@ -0,0 +1,53 @@
+package com.mux.video.upload.api
+
+/**
+ * Helper class for parsing [Result] objects from [MuxUpload] in Java.
+ *
+ * Kotlin callers can use the [Result] API as normal
+ */
+class UploadResult {
+
+  companion object {
+    /**
+     * Returns true of the upload was successful,
+     * Returns false if the upload wasn't successful, or if the passed object wasn't a [Result]
+     */
+    @JvmStatic
+    fun isSuccessful(result: Result<MuxUpload.Progress>): Boolean {
+      @Suppress("USELESS_IS_CHECK") // java interprets inline classes like Result as Object
+      return if (result is Result) {
+        result.isSuccess
+      } else {
+        false
+      }
+
+    }
+
+    /**
+     * Returns the final Progress update from [MuxUpload]'s [Result] if if was successful
+     * Returns `null` if the upload was not successful, or if the passed object wasn't a result
+     */
+    @JvmStatic
+    fun getFinalProgress(result: Result<MuxUpload.Progress>): MuxUpload.Progress? {
+      @Suppress("USELESS_IS_CHECK") // java interprets inline classes like Result as Object
+      return if (result is Result) {
+       result.getOrNull()
+      } else {
+        null
+      }
+    }
+
+    /**
+     * If the Result was not successful, returns the Exception that caused the failure
+     */
+    @JvmStatic
+    fun getError(result: Result<MuxUpload.Progress>): Throwable? {
+      @Suppress("USELESS_IS_CHECK") // java interprets inline classes like Result as Object
+      return if (result is Result) {
+        result.exceptionOrNull()
+      } else {
+        null
+      }
+    }
+  }
+}
