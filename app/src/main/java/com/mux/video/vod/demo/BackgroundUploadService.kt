371753a66544fa557440077f75eef6140a08a99d
Emily Dixon <edixon@mux.com>
2024-06-10 10:00:35 -0700

    up

--- app/src/main/java/com/mux/video/vod/demo/BackgroundUploadService.kt
+++ app/src/main/java/com/mux/video/vod/demo/BackgroundUploadService.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import com.mux.video.upload.api.MuxUpload
+import com.mux.video.upload.api.MuxUploadManager
 import com.mux.video.upload.api.UploadEventListener
 import com.mux.video.upload.api.UploadStatus
 
@@ -12,10 +13,11 @@ class BackgroundUploadService : Service() {
 
   companion object {
     const val ACTION_START = "start"
+    const val NOTIFICATION_ID = 2001
   }
 
   private var uploads = listOf<MuxUpload>()
-  private val statusListenersByFile = mutableMapOf<String, UploadListListener>()
+  private var uploadListListener: UploadListListener? = null
 
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
     val action = intent?.action
@@ -23,9 +25,14 @@ class BackgroundUploadService : Service() {
     } else {
       throw RuntimeException("Unknown action")
     }
-    // todo - listen for updates
-    // todo - foreground service
-    // todo - stop listening on destroy
+
+    // can be commanded to start arbitrary number of times
+    if (uploadListListener == null) {
+      val lis = UploadListListener()
+      this.uploadListListener = lis
+      MuxUploadManager.addUploadsUpdatedListener(lis)
+    }
+
     return super.onStartCommand(intent, flags, startId)
   }
 
@@ -33,19 +40,21 @@ class BackgroundUploadService : Service() {
     return MyBinder()
   }
 
-  private fun notify(uploads: List<MuxUpload>) {
-
+  override fun onDestroy() {
+    uploadListListener?.let { MuxUploadManager.removeUploadsUpdatedListener(it) }
   }
 
-  private fun observeUpload(upload: MuxUpload) {
-
+  private fun notify(uploads: List<MuxUpload>) {
+    // todo - Manage foreground-iness: startForeground when there are running uploads, else don't
+    // todo - Two notification styles: 1 video and many videos
+    // todo - notification can't be swiped while in-progress (but provide cancel btns)
+    // todo - cancel notification if no uploads are running
+    // todo - show new notification for completion (clear count of completed uploads when both notifs are gone)
   }
 
   private fun updateUploadList(uploads: List<MuxUpload>) {
-    this.uploads.forEach { it.setProgressListener(null) }
-
-    val currentUploads = uploads.union(this.uploads)
-    this.uploads = uploads
+    this.uploads = uploads.toList()
+    notify(uploads)
   }
 
   private inner class UploadListListener: UploadEventListener<List<MuxUpload>> {
